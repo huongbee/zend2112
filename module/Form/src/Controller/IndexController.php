3cd623e1c4dbcabe5da82b685f6b4c1a08938457
Huong <you@example.com>
2018-01-25 18:56:41 +0700

    inputText

--- module/Form/src/Controller/IndexController.php
+++ module/Form/src/Controller/IndexController.php
@@ -2,10 +2,13 @@
 namespace Form\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Form\Form\FormElement;
 
 class IndexController extends AbstractActionController{
     function indexAction(){
-        $view = new ViewModel();
+        $form = new FormElement;
+
+        $view = new ViewModel(['forms'=>$form]);
         $view->setTemplate('form/element/index');
         return $view;
     }
--- module/Form/src/Form/FormElement.php
+++ module/Form/src/Form/FormElement.php
@@ -1,4 +1,5 @@
 <?php
+namespace Form\Form;
 use Zend\Form\Element;
 use Zend\Form\Form;
 
@@ -6,19 +7,40 @@ class FormElement extends Form{
     function __construct(){
         parent::__construct();
 
+            // $this->setAttributes([
+            //     'method'=>"GET",
+            //     'action'=>"/add/user"
+            // ]);
+
+
         //textbox
         /**
          * Fullname: <input name="fullname" >
          */
         $fullname = new Element('fullname');
-        $fullname->setLabel("Fullname: ");
+        $fullname->setLabel("Họ tên: ")
+                ->setLabelAttributes([
+                    'class'=>"col-sm-2"
+                ]);
         $fullname->setAttribute('placeholder',"Enter Fullname");
         $fullname->setAttributes([
             'id'=>'fullname',
+            'class'=>'form-control',
+            "value"=>"Khoa Phạm"
+        ]);
+        $this->add($fullname);
+
+        $name = new Element\Text('name');
+        $name->setLabel("Username: ")
+                ->setLabelAttributes([
+                    'class'=>"col-sm-2"
+                ]);
+        $name->setAttribute('placeholder',"Enter Username");
+        $name->setAttributes([
+            'id'=>'name',
             'class'=>'form-control'
         ]);
-
-        $this->add($fullname);
+        $this->add($name);
 
 
     }
