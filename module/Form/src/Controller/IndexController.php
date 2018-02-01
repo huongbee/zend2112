d24b08c9934ebcde720b1eccb3249ceac603d62e
Huong <you@example.com>
2018-02-01 19:17:06 +0700

    zend-validator

--- module/Form/src/Controller/IndexController.php
+++ module/Form/src/Controller/IndexController.php
@@ -3,6 +3,8 @@ namespace Form\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Form\Form\FormElement;
+use Zend\Validator\StringLength;
+use Zend\Validator\InArray;
 
 class IndexController extends AbstractActionController{
     
@@ -19,6 +21,65 @@ class IndexController extends AbstractActionController{
         $form = new FormElement;
         $view = new ViewModel(['forms'=>$form]);
         $view->setTemplate('form/element/index');
+        return $view;
+    }
+
+    function validatorAction(){
+        $form = new FormElement;
+        //fullname 5-20 words
+        $request = $this->getRequest();
+        if($request->isPost()){
+            $dataInput = $this->params()->fromPost();
+            $fullname = $dataInput['fullname'];
+            $subject = $dataInput['subject'];
+
+            $validatorFullname = new StringLength([
+                'min'=>5,
+                'max'=>20
+            ]);
+            //$form->setData($dataInput);
+            if($validatorFullname->isValid($fullname)){
+                //valid
+                echo "Valid Fullname!";
+                echo "<br>";
+                echo $fullname;
+                echo "<br>String legnth: ";
+                echo $length = $validatorFullname->getStringLength();
+            }
+            else{ //invalid
+                $messages = $validatorFullname->getMessages();
+                foreach($messages as $error){
+                    echo $error.'<br>';
+                }
+            }
+            
+            $validatorSubject = new InArray([
+                'haystack'=>['php', 'js','html']
+            ]);
+
+            foreach($subject as $monhoc){
+                if($validatorSubject->isValid($monhoc)){
+                    //valid
+                    echo "<hr>";
+                    echo "Valid Subject!";
+                    echo "<br>";
+                    echo($monhoc);
+                    echo "<br>";
+                }
+                else{ //invalid
+                    $messages = $validatorSubject->getMessages();
+                    foreach($messages as $error){
+                        echo $error.'<br>';
+                    }
+                }
+            }
+            
+            
+            
+        }
+        
+        $view = new ViewModel(['forms'=>$form]);
+        $view->setTemplate('form/element/validator');
         return $view;
     }
 
