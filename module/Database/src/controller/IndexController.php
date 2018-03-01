a765f130513c2d6b6390e623746f0f4a27493d11
Huong <you@example.com>
2018-03-01 18:56:29 +0700

    zend-db

--- module/Database/src/controller/IndexController.php
+++ module/Database/src/controller/IndexController.php
@@ -2,11 +2,29 @@
 namespace Database\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Db\Adapter\Adapter;
 
 class IndexController extends AbstractActionController{
 
     function indexAction(){
-        echo "Hello";
+        $adapter = new Adapter([
+            'driver'   => 'Pdo_Mysql',
+            'database' => 'zend2112',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8'
+        ]);
+
+        $sql = "SELECT * FROM User where id=? and name=?";
+        //$stmt = $adapter->query($sql);
+        $stmt = $adapter->createStatement($sql,[1,'Huong2']);
+        $result = $stmt->execute();
+
+        foreach($result as $user){
+            echo "<pre>";
+            print_r($user);
+            echo "</pre>";
+        }
         return false;
     }
 }
