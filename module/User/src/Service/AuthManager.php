5703a1e77233193ac57d5dd8ea14ac5b637a461f
Huong <admin@Apples-MacBook-Pro.local>
2018-04-07 18:40:35 +0700

    AuthManager

--- module/User/src/Service/AuthManager.php
+++ module/User/src/Service/AuthManager.php
@@ -1,6 +1,35 @@
 <?php
-//login
-//logout
+namespace User\Service;
+use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Result;
 
+
+class AuthManager{
+
+    public $authenticationService;
+    public $sessionManager;
+
+    function __construct($authenticationService, $sessionManager){
+        $this->authenticationService = $authenticationService;
+        $this->sessionManager = $sessionManager;
+    }
+
+    function login($email,$password){
+        if($this->authenticationService->hasIdentity()){
+            //return -1; //da dang nhap
+            throw new \Exception('Bạn đã đăng nhập');
+        }
+        $authAdapter = $this->authenticationService->getAdapter();
+        $authAdapter->setEmail($email);
+        $authAdapter->setPassword($password);
+        $result = $authAdapter->authenticate();
+        if($result->getCode() === Result::SUCCESS){
+            //luu tru session   
+            $this->sessionManager->rememberMe(86400*2); //2days
+        }
+        return $result;
+
+    }
+}
 
 ?>
--- module/User/src/Service/Factory/AuthAdapterFactory.php
+++ module/User/src/Service/Factory/AuthAdapterFactory.php
@@ -1,4 +1,18 @@
 <?php
+namespace User\Service\Factory;
+use User\Service\AuthAdapter;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\InvokableFactory;
+use Zend\ServiceManager\ServiceManager;
+use Zend\ServiceManager\Factory\FactoryInterface;
 
+class AuthAdapterFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        return new AuthAdapter($entityManager);
+    }
+}
 
 ?>
